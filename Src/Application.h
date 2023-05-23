4671168a9bcfe2b38544acd08f7aeb8219a3671c
rernamp <renamp100@gmail.com>
2023-05-23 23:23:21 +0300

    fix fft

--- Src/Application.h
+++ Src/Application.h
@@ -22,6 +22,16 @@ namespace UDA {
 
 			fillBufferFromFile(pathToInputSignal, _inputSignal);
 			fillBufferFromFile(pathToNoiseSignal, _noiseSignal);
+			std::size_t sizeOfFiles = std::min(_noiseSignal[0].size(), _inputSignal[0].size());
+			auto delUnusedBuffer = [](std::vector<float>& buffer, std::size_t size) {
+				if (buffer.size() != size) {
+					buffer.erase(buffer.begin() + size, buffer.begin() + buffer.size());
+				}
+			};
+
+			delUnusedBuffer(_inputSignal[0], sizeOfFiles);
+			delUnusedBuffer(_noiseSignal[0], sizeOfFiles);
+			
 			_fs = _audioWav.getSampleRate();
 		}
 
@@ -38,9 +48,11 @@ namespace UDA {
 
 			univector<complex<float>> fftNoise = fft(_noiseSignal);
 
+			float tauByOneElement = (_distanceBetweenElement*sind(_angleOfArrivalNoise))/(_soundSpeed);
 			for (std::size_t i = 0; i < resultsBuffer.size(); i++) {
-				resultsBuffer[i] = shiftSpectr(fftNoise, 10, _fs);
+				resultsBuffer[i] = shiftSpectr(fftNoise, tauByOneElement * i, _fs);
 			}
+			
 		}
 
 		kfr::univector<kfr::complex<float>> fft(const AudioFileType::AudioBuffer& buffer) {
@@ -66,8 +78,8 @@ namespace UDA {
 				auto fTau = ((2 + 2 + nOne - 1)/2) + i*nOne;
 
 				kfr::complex<float> shiftValue = 0.0f;
-				for (std::size_t j = 2 * i * nOne; i < (1 + nOne + i * nOne); i++) {
-					shiftValue = inputSpectr[j] * kfr::exp(- complex<float>(0, -1) * 2 * kfr::constants<float>::pi * fTau * tauShift * fs / inputSpectr.size());
+				for (std::size_t j = 2 * i * nOne; j < (1 + nOne + i * nOne); j++) {
+					shiftValue = inputSpectr[j] * kfr::cexp(- complex<float>(0, -1) * 2 * kfr::constants<float>::pi * fTau * tauShift * fs / inputSpectr.size());
 					result.push_back(shiftValue);
 				}
 			}
@@ -96,7 +108,7 @@ namespace UDA {
 		}
 
 		const std::size_t _numberElements = 4;
-		const float soundSpeed = 330.0f;
+		const float _soundSpeed = 330.0f;
 		const float _distanceBetweenElement = 0.1f;
 		const float _angleOfArrivalNoise = 60;
 
