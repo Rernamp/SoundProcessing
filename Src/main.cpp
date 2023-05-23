b3fe83426672235e16991d119d5effb4694fc9a6
rernamp <renamp100@gmail.com>
2023-05-23 08:05:59 +0300

    fix build

--- Src/main.cpp
+++ Src/main.cpp
@@ -7,8 +7,11 @@ int main(int argc, char* argv[]) {
     using namespace UDA;
 
     if (argc != 4) {
+        std::cout << "Infalid number input parameter" << std::endl;
         return -1;
     }
+
+    std::cout << "Start application" << std::endl;
 
     Application app {argv[1], argv[2], argv[3]};
 
